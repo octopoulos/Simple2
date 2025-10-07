1f772a5e8e61a9f8173dada7037cc0b7e12a2447
octopoulos <polluxyz@gmail.com>
2025-10-07 19:20:03 +0300

    Matrix full info

--- src/app/version.cpp
+++ src/app/version.cpp
@@ -1,7 +1,7 @@
 // version.cpp
 // @author octopoulos
-// @version 2025-10-02
+// @version 2025-10-03
 
 #include "stdafx.h"
 
-std::string VERSION = "Simple 20251002";
+std::string VERSION = "Simple 20251003";
